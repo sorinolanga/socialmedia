40f862b5f5a54c228756d49e869dd302a7cd94b5
Eng.Langa <sameemiracle@gmail.com>
2025-09-24 15:36:04 +0200

    initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'socialmedia/socialmedia',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7cb7fe3b9cadfc57a5509ca73660781e5465d997',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'socialmedia/socialmedia' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7cb7fe3b9cadfc57a5509ca73660781e5465d997',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
